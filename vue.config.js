656ddbe369b40888ad43e1e18053073c5522b5a5
sixpeteunder <plenjo15@gmail.com>
2021-08-10 14:55:55 +0300

    Make android-chrome icons transparent

--- vue.config.js
+++ vue.config.js
@@ -12,11 +12,13 @@ module.exports = {
           src: "/android-chrome-192x192.png",
           sizes: "192x192",
           type: "image/png",
+          purpose: "maskable any",
         },
         {
           src: "/android-chrome-512x512.png",
           sizes: "512x512",
           type: "image/png",
+          purpose: "maskable any",
         },
       ],
     },
